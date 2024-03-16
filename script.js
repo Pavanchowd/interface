70728c4393374c07a0022210f88e7edf1ca7d411
Pavanchowd <149993352+Pavanchowd@users.noreply.github.com>
2024-03-16 22:17:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+     const employee={
+      caltax(){
+        console.log("you need to calculate the tax at 30%");
+      }
+     }
+     const pavankumar={
+      salary:50000
+     }
+     pavankumar.__proto__=employee;
